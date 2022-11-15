283230004b75aba545efb7e9d4a5b607c2189831
junhara <junpeih88@gmail.com>
2022-11-15 09:28:10 +0900

    Fixed comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'main#index'
 
-  # get "sign_up", to: "users#new"
-  # post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
+  post "sign_up", to: "users#create"
 
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
