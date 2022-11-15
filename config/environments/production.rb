ed47635330224ec58809849a6fcde8201b41ad35
junhara <junpeih88@gmail.com>
2022-11-15 10:30:53 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,6 +117,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  
-  ENV['DEPLOY_TASKS']='NODE_OPTIONS=openssl-legacy-provider bundle exec rake webpacker:compile'
 end
